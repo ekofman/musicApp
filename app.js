98cccdcc025a8340151d97ccebc5ee0c3cd85e5b
Eric Kofman <eric.kofman@opower.com>
2014-08-04 20:57:32 -0700

    added recorder code

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+// Database
+var mongo = require('mongoskin');
+var db = mongo.db("mongodb://localhost:27017/recordings", {native_parser:true});
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +23,11 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Make our db accessible to our router
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
 app.use('/', routes);
 app.use('/users', users);
 
